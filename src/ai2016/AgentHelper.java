81378546bc0335166e8df3aa639eaea6593eeac4
Kharisnawan <Kharisnawan@wlan-145-94-142-195.wlan.tudelft.nl>
2016-10-05 13:51:08 +0200

    add import files

--- src/ai2016/AgentHelper.java
+++ src/ai2016/AgentHelper.java
@@ -1,5 +1,19 @@
 package ai2016;
 
+import java.util.List;
+
+import negotiator.AgentID;
+import negotiator.Bid;
+import negotiator.Deadline;
+import negotiator.actions.Accept;
+import negotiator.actions.Action;
+import negotiator.actions.Offer;
+import negotiator.issue.Issue;
+import negotiator.parties.AbstractNegotiationParty;
+import negotiator.session.TimeLineInfo;
+import negotiator.utility.AbstractUtilitySpace;
+
+
 public class AgentHelper {
 	public static double CalculateUtility(){
 		double result = 0.0;
